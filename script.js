77b93da31ad50d6806188e3112a1991977842f09
John Doe <johndoe@email.com>
2024-02-07 14:55:55 -0500

    Updated Recaptha

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-function beforesubmit(){
-    debugger;
-    let outputdate = document.querySelector('.outputdate');
-    let inputdate = document.querySelector('.inputdate');
-    console.log('Inputdate value: ', inputdate.value);//* convert into date form
+let capthcheck = false;
 
-    let formatedDate = new Date(inputdate.value).toLocaleDateString('en-US');
-    outputdate.value = formatedDate;
+function beforesubmit(event){
+    
+    if (capthcheck) {
+        let outputdate = document.querySelector('.outputdate');
+        let inputdate = document.querySelector('.inputdate');
+        console.log('Inputdate value: ', inputdate.value);//* convert into date form
+    
+        let formatedDate = new Date(inputdate.value).toLocaleDateString('en-US');
+        outputdate.value = formatedDate;
+    } else {
+        alert(`Please check the re-acptha box to submit the lead`);
+        event.preventDefault();
+    }
+
 
 };
 
@@ -14,5 +22,7 @@ function timestamp() {
      if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
      elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
 
-
+function capthachasuccess() {
+    capthcheck = true;
+}
 
